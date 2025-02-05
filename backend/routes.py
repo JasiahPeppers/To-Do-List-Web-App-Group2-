60aaa21caccbee4d8591ba73e07031a70a3d5bf4
JasiahPeppers <160697523+JasiahPeppers@users.noreply.github.com>
2025-02-05 11:18:41 -0500

    Update routes.py

--- backend/routes.py
+++ backend/routes.py
@@ -51,11 +51,10 @@ def add_task():
 def update_task(task_id):
     data = request.json
     task = Task.query.get_or_404(task_id)
-    task.task = data.get('task', task.task)
-    task.desc = data.get('desc', task.desc)
-    task.priority = data.get('priority', task.priority)
+    
+    # Ensure that the 'status' is handled as a string
     task.status = data.get('status', task.status)
-    task.task_date = data.get('task_date', task.task_date)
+    
     db.session.commit()
     return jsonify({'message': 'Task updated successfully'}), 200
 
