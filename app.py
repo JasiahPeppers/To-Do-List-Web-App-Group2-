30c942a7afedbfe9f64c5179112a3eb28ac7b25c
Brett Lewis <139373745+SurfPunk407@users.noreply.github.com>
2025-02-07 14:25:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import os
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///tasks.db')
 #app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
