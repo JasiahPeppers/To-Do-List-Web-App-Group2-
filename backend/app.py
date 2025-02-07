cfd1062d3997eecdac757f4d5ead376b20ef578c
Brett Lewis <139373745+SurfPunk407@users.noreply.github.com>
2025-02-07 12:18:19 -0500

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,30 @@
 
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from models import db, Task  # Import db and Task from models
+from routes import list_routes
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Initialize the database (using the db instance)
+db.init_app(app)
+
+# Allows cross communication
+CORS(app)  
+
+
+# From routes.py for all the route methods
+list_routes(app)
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()  # Create the table
+        print("Database created successfully")
+    app.run()  #app.run(debug=True)
+
+# This imports everything, points to where the DB is, starts or creates the database 
+# when the Flask program is started, and is the starting point of the backend.
