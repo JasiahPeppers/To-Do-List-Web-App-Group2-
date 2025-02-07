e7c95ca7b9c6ac63c61f619d0d68395cb04d5e24
Brett Lewis <139373745+SurfPunk407@users.noreply.github.com>
2025-02-07 14:16:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from models import db, Task  # Import db and Task from models
 from routes import list_routes
+import os
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///tasks.db')
+#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Initialize the database (using the db instance)
 db.init_app(app)
