5856ad491bb29c8b3ed232b16ef7bb767102e9a9
JasiahPeppers <160697523+JasiahPeppers@users.noreply.github.com>
2025-02-08 10:23:00 -0500

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,3 @@
-
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -6,7 +5,7 @@ db = SQLAlchemy()
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     task = db.Column(db.String(50), nullable=True)
-    desc = db.Column(db.String(255), nullable=True)
+    description = db.Column(db.String(255), nullable=True)  # field name is 'description'
     priority = db.Column(db.String(50), nullable=True)
     status = db.Column(db.Boolean, default=True)
     task_date = db.Column(db.String(20), nullable=True)
@@ -15,10 +14,11 @@ class Task(db.Model):
         return {
             "id": self.id,
             "task": self.task,
-            "desc": self.desc,
+            "description": self.description,  # Change 'desc' to 'description'
             "priority": self.priority,
             "status": self.status,
             "task_date": self.task_date
         }
+
 
 # This defines what's in the table and then turns it into a dictionary for easier JSON handling.
