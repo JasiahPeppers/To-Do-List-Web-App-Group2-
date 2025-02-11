0f0d54f8a397da3a7a2a531b0f22558051f7f2ac
JasiahPeppers <160697523+JasiahPeppers@users.noreply.github.com>
2025-02-11 01:59:55 -0500

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -4,11 +4,14 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Task(db.Model):
+    """
+    Task Model represents the tasks in the database.
+    """
     id = db.Column(db.Integer, primary_key=True)
     task = db.Column(db.String(50), nullable=False)
     description = db.Column(db.String(255), nullable=True)
     priority = db.Column(db.String(50), nullable=True)
-    status = db.Column(db.Boolean, default=True)
+    status = db.Column(db.Boolean, default=True)  # Default to True (e.g., task is active)
     task_date = db.Column(db.String(20), nullable=True)
 
     def __init__(self, task, description=None, priority=None, status=True, task_date=None):
@@ -19,6 +22,9 @@ class Task(db.Model):
         self.task_date = task_date
 
     def to_dictionary(self):
+        """
+        Convert Task object to dictionary format.
+        """
         return {
             "id": self.id,
             "task": self.task,
